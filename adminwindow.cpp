f39074b639d3d26fbaf570d15c3713225fe8e5ab
Shasank12345 <shasanksingh000@gmail.com>
2024-12-19 15:39:27 +0545

    modified

--- adminwindow.cpp
+++ adminwindow.cpp
@@ -19,22 +19,3 @@ AdminWindow::~AdminWindow()
 {
     delete ui;
 }
-
-void AdminWindow::on_pushButton_clicked()
-{
-    QPushButton *p1=new QPushButton(this);
-    p1->setText("ADD STUDENT");
-
-    QPushButton *p2=new QPushButton(this);
-
-    p2->setText("DELETE STUDENT");
-
-    QHBoxLayout *p=new QHBoxLayout(this);
-    p->addWidget(p1);
-    p->addWidget(p2);
-
-
-
-
-}
-
