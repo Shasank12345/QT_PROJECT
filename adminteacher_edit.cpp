51cbc0d5b18a80b69f6bd96861947344fd3df642
ghanshyamkhatri1 <ghanashyamkhatri958@gmail.com>
2024-12-28 14:40:24 +0545

    admin window update

--- adminteacher_edit.cpp
+++ adminteacher_edit.cpp
@@ -6,9 +6,58 @@ AdminTeacher_Edit::AdminTeacher_Edit(QWidget *parent)
     , ui(new Ui::AdminTeacher_Edit)
 {
     ui->setupUi(this);
+    connect(ui->Add_data, &QPushButton::clicked, this, &AdminTeacher_Edit::on_Add_data_clicked);
+     connect(ui->Add_data, &QPushButton::clicked, this, &AdminTeacher_Edit::on_Edit_clicked);
+      connect(ui->Add_data, &QPushButton::clicked, this, &AdminTeacher_Edit::on_Delet_clicked);
+     connect(ui->Add_data, &QPushButton::clicked, this, &AdminTeacher_Edit::on_addback_clicked);
+     connect(ui->Add_data, &QPushButton::clicked, this, &AdminTeacher_Edit::on_editback_clicked);
+    connect(ui->Add_data, &QPushButton::clicked, this, &AdminTeacher_Edit::on_deletback_clicked);
+
 }
 
 AdminTeacher_Edit::~AdminTeacher_Edit()
 {
     delete ui;
 }
+
+
+void AdminTeacher_Edit::on_Add_data_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void AdminTeacher_Edit::on_Edit_clicked()
+{
+     ui->stackedWidget->setCurrentIndex(2);
+}
+
+void AdminTeacher_Edit::on_Delet_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+
+
+
+void AdminTeacher_Edit::on_addback_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void AdminTeacher_Edit::on_editback_clicked()
+{
+      ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+
+
+void AdminTeacher_Edit::on_deletback_clicked()
+{
+   ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+
--- adminteacher_edit.h
+++ adminteacher_edit.h
@@ -1,6 +1,5 @@
 #ifndef ADMINTEACHER_EDIT_H
 #define ADMINTEACHER_EDIT_H
-
 #include <QDialog>
 
 namespace Ui {
@@ -15,8 +14,38 @@ public:
     explicit AdminTeacher_Edit(QWidget *parent = nullptr);
     ~AdminTeacher_Edit();
 
+private slots:
+    void on_Add_clicked();
+
+    void on_Prev_clicked();
+
+    void on_Edit_clicked();
+
+    void on_View_3_clicked();
+
+    void on_Back_clicked();
+
+    void on_delet_clicked();
+
+    void on_Back_1_clicked();
+
+    void on_back_2_clicked();
+
+    void on_Back_3_clicked();
+
+    void on_Delet_clicked();
+
+    void on_addback_clicked();
+
+    void on_editback_clicked();
+
+    void on_deletback_clicked();
+
+    void on_Add_data_clicked();
+
 private:
     Ui::AdminTeacher_Edit *ui;
+
 };
 
 #endif // ADMINTEACHER_EDIT_H
