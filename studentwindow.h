943bc62e2b4b94b697606a2bcb348bccf8345b94
shaswatpoudel123 <shaswatpoudel100@gmail.com>
2025-02-06 22:34:21 +0545

    student page update

--- studentwindow.h
+++ studentwindow.h
@@ -15,6 +15,15 @@ public:
     explicit StudentWindow(QWidget *parent = nullptr);
     ~StudentWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::StudentWindow *ui;
 };
